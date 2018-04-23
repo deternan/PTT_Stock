3315165f964514e7378290ccfacbacd46e19d00c
deternan <chke@iir.csie.ncku.edu.tw>
2018-04-23 20:53:34 +0800

    get PTT Stock data (threading)

--- py/get_pttdata.py
+++ py/get_pttdata.py
@@ -1,13 +1,25 @@
 # coding: utf-8
 
 import os
+import threading
+import time
 
 '''
-Created on April 23, 2018 00:54 AM
+Created on April 23, 2018 08:43 PM
 
 @author: Phelps
 '''
+
+def workerthread(num):    
+    #os.system("python crawler.py -b Stock -i "+str(num)+" "+str(num)+ " &")
+    print(num)
+    return
+
+threads = []
 for x in range(3000, 3002):
-    os.system("python crawler.py -b Stock -i "+str(x)+" "+str(x)+ " &")
-    #print("python crawler.py -b Stock -i ",x ,x)
-    #print(x)
+    t = threading.Thread(target=workerthread, args=(x,))
+    threads.append(t)
+    t.start()
+    'delay'
+    time.sleep(5)
+
