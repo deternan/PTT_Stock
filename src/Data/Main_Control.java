75ed3f148cefceba385a92c73ac9d0760490741e
deternan <chke@iir.csie.ncku.edu.tw>
2018-06-03 02:27:21 +0800

    added Testing functions

--- src/Data/Main_Control.java
+++ src/Data/Main_Control.java
@@ -21,23 +21,29 @@ public class Main_Control extends Parameters
 	// TPEX
 	private Vector tpex_id = new Vector();
 	private Vector tpex_name = new Vector();
+	// id & value
+	private Vector id = new Vector();
+	private Vector value = new Vector();
 	
-	
-	StandardData_Text SD;
-	
+	//StandardData_Text SD;
+	StandardData_Text_Testing SD_testing;
 	
 	public Main_Control() throws Exception
 	{		
 		Read_TWSE_Info();
 		Read_TPEX_Info();
 		
+		Read_Current_value();
+		
 //		System.out.println(twse_id.size());
 //		System.out.println(twse_name.size());		
 //		System.out.println(tpex_id.size());
 //		System.out.println(tpex_name.size());
+//		System.out.println(id.size());
+//		System.out.println(value.size());
 		
-		SD = new StandardData_Text(twse_id, twse_name, tpex_id, tpex_name);
-		
+		//SD = new StandardData_Text(twse_id, twse_name, tpex_id, tpex_name);
+		SD_testing = new StandardData_Text_Testing(twse_id, twse_name, tpex_id, tpex_name, id, value);
 		
 		
 		// ------------
@@ -60,6 +66,9 @@ public class Main_Control extends Parameters
 			twse_id.add(array_temp[0]);
 			twse_name.add(array_temp[1]);			
 		}
+		
+		fr.close();
+		bfr.close();
 	}
 	
 	private void Read_TPEX_Info() throws Exception
@@ -78,9 +87,28 @@ public class Main_Control extends Parameters
 			tpex_id.add(array_temp[0]);
 			tpex_name.add(array_temp[1]);			
 		}
+		
+		fr.close();
+		bfr.close();
 	}
 	
-	
+	private void Read_Current_value() throws Exception
+	{
+		String Line = "";
+		FileReader fr = new FileReader(value_path + value_filename);
+		BufferedReader bfr = new BufferedReader(fr);
+		
+		String array_temp[];
+		while ((Line = bfr.readLine()) != null)
+		{
+			array_temp = Line.split("\t");
+			id.add(array_temp[0]);
+			value.add(array_temp[2]);
+		}
+		
+		fr.close();
+		bfr.close();
+	}
 	
 	public static void main(String[] args)
 	{
--- src/Data/Parameters.java
+++ src/Data/Parameters.java
@@ -15,6 +15,9 @@ public class Parameters
 	// PTT source
 	protected String ppt_path = path;
 	protected String ppt_filename = "ptt_text.txt";
+	// Current values
+	protected String value_path = path;
+	protected String value_filename = "2018-06-01_values.txt";
 	// Text matching
 	final List<String> pageTypes_filter = new ArrayList<>(Arrays.asList("標的", "心得", "請益", "投顧"));
 	final List<String> TagsTypes_filter = new ArrayList<>(Arrays.asList("多", "空"));
