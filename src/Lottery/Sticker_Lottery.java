15ee4b949b9d720d28d8eb1b37a68b7120eaa9a9
deternan <chke@iir.csie.ncku.edu.tw>
2018-06-21 01:23:00 +0800

    Line sticker Lottery

--- src/Lottery/Sticker_Lottery.java
+++ src/Lottery/Sticker_Lottery.java
@@ -4,14 +4,18 @@ package Lottery;
  * PTT Json parser & Lottery 
  * 
  * version: June 20, 2018 00:40 AM
- * Last revision: June 20, 2018 01:15 AM
+ * Last revision: June 21, 2018 00:54 AM
  * 
  * Author: Chao-Hsuan Ke
+ * E-mail: phelpske dot dev at gmail dot com
  * 
  */
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.OutputStreamWriter;
 import java.util.Random;
 import java.util.Vector;
 
@@ -20,21 +24,26 @@ import org.json.JSONObject;
 
 public class Sticker_Lottery 
 {
+	// Get source
 	private String read_file_path = "C:\\Users\\Barry\\Desktop\\";
 	private String read_file_name = "M.1529344339.A.0DB.json";
 	private BufferedReader bfr;
-
+	// output
+	private String outputAll_folder = read_file_path;
+	private String outputAll_file = "allpush.txt";	
+	private BufferedWriter writer;
+	
 	private Vector All_date = new Vector();
 	private Vector All_userid = new Vector();
 	private Vector All_content = new Vector();
 	
 	// Lottery
 	private int L_Number = 20;
-	private int min = 0;
+	private int min = 1;
 	private int max;
 	private int randomNum;
 		// Random
-		int seed = 1128;
+		int seed = 0616;	// lopm27
 	
 	public Sticker_Lottery() throws Exception
 	{
@@ -43,10 +52,8 @@ public class Sticker_Lottery
 		BufferedReader bfr = new BufferedReader(fr);
 		
 		JSONObject obj;
-		while ((Line = bfr.readLine()) != null) {
-			
-			obj = new JSONObject(Line);			
-			//System.out.println(obj.get("messages"));
+		while ((Line = bfr.readLine()) != null) {			
+			obj = new JSONObject(Line);						
 			ArrayParser(obj.get("messages").toString());
 		}
 		
@@ -64,12 +71,14 @@ public class Sticker_Lottery
 		for(int i=0; i<jsonarray.length(); i++)
 		{			
 			pusgobj = new JSONObject(jsonarray.get(i).toString());
-			//System.out.println(i+"	"+pusgobj.get("push_ipdatetime")+"	"+pusgobj.get("push_userid"));
+			//System.out.println(i+"	"+pusgobj.get("push_ipdatetime")+"	"+pusgobj.get("push_userid")+"	"+pusgobj.get("push_content"));
 			All_date.add(pusgobj.get("push_ipdatetime"));
 			All_userid.add(pusgobj.get("push_userid"));
-			All_content.add(pusgobj.get("push_content"));
+			All_content.add(pusgobj.get("push_content"));						
 		}
 		
+		// output
+		output_all();
 	}
 	
 	private void Lottery(int L_Num)
@@ -82,8 +91,19 @@ public class Sticker_Lottery
 			randomNum = min + rand.nextInt(max);
 			//System.out.println((i+1)+"	"+randomNum);
 			System.out.println((i+1)+"	"+randomNum+"	"+All_date.get(randomNum)+"	"+All_userid.get(randomNum)+"	"+All_content.get(randomNum));
+		}	
+	}
+	
+	private void output_all() throws Exception
+	{
+		writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputAll_folder + outputAll_file), "utf-8"));
+		
+		for(int i=0; i<All_userid.size(); i++)
+		{
+			writer.write((i+1)+"	"+All_date.get(i)+"	"+All_userid.get(i)+"	"+All_content.get(i)+"\n");
 		}
 		
+		writer.close();
 	}
 	
 	public static void main(String args[])
