98888ba86837a7ff4546d66a1d361104e454bf9f
Chao-Hsuan Ke <chke@iir.csie.ncku.edu.tw>
2019-06-22 21:36:24 +0800

    add twse reading

--- src/main/java/GUI/ReadCompanySource.java
+++ src/main/java/GUI/ReadCompanySource.java
@@ -2,9 +2,10 @@ package GUI;
 
 /*
  * Sotck GUI
+ * a. Read Company Information
  * 
  * version: June 19, 2019 10:29 PM
- * Last revision: June 20, 2019 07:29 AM
+ * Last revision: June 22, 2019 09:26 PM
  * 
  * Author : Chao-Hsuan Ke
  * 
@@ -26,14 +27,18 @@ public class ReadCompanySource
 	// Data
 	Vector tpexVec_id = new Vector();
 	Vector tpexVec_name = new Vector();
+	Vector twseVec_id = new Vector();
+	Vector twseVec_name = new Vector();
 	
 	
 	public ReadCompanySource() throws Exception
 	{
 		// Read tpex
 		ReadCompanylist(tpexlist, tpexVec_id, tpexVec_name);
+		// Read TWSE
+		ReadCompanylist(twselist, twseVec_id, twseVec_name);
 		
-		System.out.println(tpexVec_id.size()+"	"+tpexVec_name.size());
+		//System.out.println(tpexVec_id.size()+"	"+tpexVec_name.size()+"	"+twseVec_id.size()+"	"+twseVec_name.size());
 	}
 	
 	private void ReadCompanylist(String path, Vector idVec, Vector nameVec) throws Exception {
