163f8bc27f7487388b1ff6071276fd5992eb327c
Chao-Hsuan Ke <chke@iir.csie.ncku.edu.tw>
2019-06-24 23:36:06 +0800

    get value (Java)

--- src/main/java/GUI/GetValues.java
+++ src/main/java/GUI/GetValues.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
 
@@ -30,10 +31,35 @@ public class GetValues
 	private JSONObject get_json = null;
 	private String jsonStrurl = "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20190624&stockNo=2388";
 	
-	public GetValues()
+	public GetValues() throws Exception
 	{
-		ReadValueJson();
+		//ReadValueJson();
+		
+		JSONObject json = readJsonFromUrl(jsonStrurl);
+	    System.out.println(json.toString());
+	    System.out.println(json.get("id"));
 	}
+	
+	private static String readAll(Reader rd) throws IOException {
+	    StringBuilder sb = new StringBuilder();
+	    int cp;
+	    while ((cp = rd.read()) != -1) {
+	      sb.append((char) cp);
+	    }
+	    return sb.toString();
+	  }
+
+	  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+	    InputStream is = new URL(url).openStream();
+	    try {
+	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+	      String jsonText = readAll(rd);
+	      JSONObject json = new JSONObject(jsonText);
+	      return json;
+	    } finally {
+	      is.close();
+	    }
+	  }
 	
 	private void ReadValueJson()
 	{
@@ -68,7 +94,12 @@ public class GetValues
 	
 	public static void main(String args[])
 	{
-		GetValues gv = new GetValues();
+		try {
+			GetValues gv = new GetValues();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
