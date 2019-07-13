16d04edbb10a634827c77549f80269800300ba36
Chao-Hsuan Ke <chke@iir.csie.ncku.edu.tw>
2019-07-14 00:20:06 +0800

    delete

--- Auto_Updated.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /mnt/newhd/eTube/Autocomplete_dev
-java -cp .:mongodb-driver-3.4.2.jar:mongodb-driver-core-3.4.2.jar:org.json.jar:json-simple-1.1.1.jar:bson-3.4.2.jar Insert_eTube_dev
