d018dc11c0dbecd1afffeafb3fedec25455737a2
Chao-Hsuan Ke <chke@iir.csie.ncku.edu.tw>
2019-07-14 09:02:52 +0800

    add value average function

--- src/main/java/GUI/DataTagging_Frame.java
+++ src/main/java/GUI/DataTagging_Frame.java
@@ -3,7 +3,7 @@ package GUI;
 /*
  * PTT Data tagging GUI
  * version: July 08, 2019 07:40 PM
- * Last revision: July 12, 2019 06:52 AM
+ * Last revision: July 14, 2019 08:59 AM
  * 
  * Author : Chao-Hsuan Ke
  * E-mail : phelpske.dev at gmail dot com
@@ -22,7 +22,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -93,6 +99,20 @@ public class DataTagging_Frame {
 		private String date;
 		private int messagesCount;
 	
+	String inputarticleId;
+	String formatdate;
+	String TWDate;
+	String formatdateAdd;
+	String TWDateAdd;
+	String addtwpday;
+	// values
+	Vector allvalueVec = new Vector();
+	// Date
+	DateFormat df = new SimpleDateFormat(Units.basic_pattern, Locale.getDefault());
+	private double onemonthAverage;
+	private double twomonthAverage;
+	private double threemonthAverage;
+	private String dateremindTag;		
 	
 	/**
 	 * Launch the application.
@@ -147,7 +167,7 @@ public class DataTagging_Frame {
 				int result = fileChooser.showOpenDialog(frame);
 				if (result == JFileChooser.APPROVE_OPTION) {
 					
-					// Company info.
+					// Loading Company info.
 					try {
 						ReadCompany();
 					} catch (Exception e2) {
@@ -160,6 +180,7 @@ public class DataTagging_Frame {
 					File selectedFile = fileChooser.getSelectedFile();
 				    // function
 					try {
+						// Loading history
 						ReadHistory rh = new ReadHistory(selectedFile.getAbsolutePath());
 						//System.out.println("Selected file: " + selectedFile.getAbsolutePath());
 						fileName_index = rh.returnfileName();
@@ -171,7 +192,7 @@ public class DataTagging_Frame {
 						filenameVec = ra.returnfilename();
 						articleIdVec = ra.returnarticleId();
 						
-						// articleIndex = 0
+						// start to load article content by articleIndex 
 						GetContentByArticleId(filenameVec.get(articleIndex).toString(), articleIdVec.get(articleIndex).toString());
 						pattern = Pattern.compile(regexTitle, Pattern.MULTILINE);
 						matcher = pattern.matcher(title);
@@ -201,19 +222,7 @@ public class DataTagging_Frame {
 							}
 							textPane_3.setText(companyvalueStr);
 						}else {
-							lblNewLabel_2.setText(date);
-							lblNewLabel_3.setText(author);
-							lblNewLabel_6.setText(title);
-							textPane_1.setText("");
-							textPane_2.setText("");
-							textPane_3.setText("");
-							textPane.setText("");
-							lblNewLabel_6.setEnabled(false);
-							
-							messagesCount = 0;
-							companyIdDisplay.clear();
-							companyNameDisplay.clear();
-							valueDisplay.clear();
+							DisplayAndClean();
 						}
 						
 						articleIndex++;
@@ -229,6 +238,7 @@ public class DataTagging_Frame {
 		
 		mnFile.add(mnload);
 		
+		// Exit button
 		JMenuItem mnexit = new JMenuItem("Exit");
 		mnexit.addActionListener(new ActionListener() {
 			@Override
@@ -316,17 +326,20 @@ public class DataTagging_Frame {
 		lblNewLabel_8.setBounds(176, 370, 78, 16);
 		frame.getContentPane().add(lblNewLabel_8);
 		
-		JLabel lblNewLabel_9 = new JLabel("1 month");
-		lblNewLabel_9.setBounds(176, 398, 78, 16);
-		frame.getContentPane().add(lblNewLabel_9);
+		JLabel lblMonth1 = new JLabel("1 month");
+		lblMonth1.setBounds(176, 398, 78, 16);
+		lblMonth1.setText("");
+		frame.getContentPane().add(lblMonth1);
 		
-		JLabel lblMonth = new JLabel("2 month");
-		lblMonth.setBounds(176, 442, 78, 16);
-		frame.getContentPane().add(lblMonth);
+		JLabel lblMonth2 = new JLabel("2 month");
+		lblMonth2.setBounds(176, 442, 78, 16);
+		lblMonth2.setText("");
+		frame.getContentPane().add(lblMonth2);
 		
-		JLabel lblMonth_1 = new JLabel("3 month");
-		lblMonth_1.setBounds(176, 491, 78, 16);
-		frame.getContentPane().add(lblMonth_1);
+		JLabel lblMonth3 = new JLabel("3 month");
+		lblMonth3.setBounds(176, 491, 78, 16);
+		lblMonth3.setText("");
+		frame.getContentPane().add(lblMonth3);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
 		rdbtnNewRadioButton.setBounds(403, 398, 141, 23);
@@ -357,12 +370,12 @@ public class DataTagging_Frame {
 					// Pattern check
 					PatternCheck(content);
 					
+					// display on board
 					lblNewLabel_2.setText(date);
 					lblNewLabel_3.setText(author);
 					lblNewLabel_6.setEnabled(true);
 					lblNewLabel_6.setText(title);
 					textPane_2.setText(content);
-					
 					String companynameStr = "";
 					for(int i=0; i<companyNameDisplay.size(); i++) {
 						companynameStr += companyNameDisplay.get(i).toString() + "\n";
@@ -378,20 +391,40 @@ public class DataTagging_Frame {
 						companyvalueStr += valueDisplay.get(i).toString() + "\n";
 					}
 					textPane_3.setText(companyvalueStr);
-				}else {
-					lblNewLabel_2.setText(date);
-					lblNewLabel_3.setText(author);
-					lblNewLabel_6.setText(title);
-					textPane_1.setText("");
-					textPane_2.setText("");
-					textPane_3.setText("");
-					textPane.setText("");
-					lblNewLabel_6.setEnabled(false);
 					
-					messagesCount = 0;
-					companyIdDisplay.clear();
-					companyNameDisplay.clear();
-					valueDisplay.clear();
+					// functions
+					if(companyIdDisplay.size() > 0) {
+						inputarticleId = companyIdDisplay.get(0).toString();
+						// date 
+						date = replaceSpace(date);
+						try {
+							formatdate = ISODateParser(date);
+						} catch (Exception e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						TWDate = convertTWDate(formatdate);
+						
+						try {
+							// add day
+							addtwpday = addTwoDate(formatdate);
+							formatdateAdd = ISODateParserZone(addtwpday);
+							TWDateAdd = convertTWDate(formatdateAdd);
+							
+							// values average
+							getValueAverageByarticleId(inputarticleId, TWDate, TWDateAdd);
+						} catch (Exception e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						
+						lblMonth1.setText(String.valueOf(onemonthAverage));
+						lblMonth2.setText(String.valueOf(twomonthAverage));
+						lblMonth3.setText(String.valueOf(threemonthAverage));
+					}
+					
+				}else {
+					DisplayAndClean();
 				}
 				
 				articleIndex++;
@@ -403,103 +436,6 @@ public class DataTagging_Frame {
 		btnSaveExit.setBounds(540, 486, 117, 29);
 		frame.getContentPane().add(btnSaveExit);
 	}
-	
-//	private void ReadAllArticles(String historyfileName, String historyarticleId) throws Exception
-//	{
-//		File folder = new File(Units.articleFolder);
-//		File[] listOfFiles = folder.listFiles();
-//		Arrays.sort(listOfFiles);
-//		
-//		for (File file : listOfFiles) {
-//		    if (file.isFile()) {
-//		    	
-//		    	if((filestartPoint == true) && (startPoint == true)) {
-//		    		StartCoolection(file.getName());
-//		    	}
-//		    	
-//		    	if(file.getName().equalsIgnoreCase(historyfileName)) {
-//	    			filestartPoint = true;
-//	    			articleIndex(historyfileName, historyarticleId, file.getName());
-//	    		}
-//		    }
-//		}
-//	}
-//	
-//	private void StartCoolection(String currentFileName) throws Exception
-//	{
-//		String Line = "";
-//		FileReader fr = new FileReader(Units.articleFolder + currentFileName);
-//		BufferedReader bfr = new BufferedReader(fr);
-//		
-//		String strTmp = "";
-//		while((Line = bfr.readLine())!=null)
-//		{	
-//			strTmp += Line;
-//		}
-//		fr.close();
-//		bfr.close();
-//		
-//		String idTmp;
-//		if(isJSONValid(strTmp)) {
-//			JSONObject obj = new JSONObject(strTmp);
-//			if(obj.has("articles")) {
-//				JSONArray jsonarray = new JSONArray(obj.get("articles").toString());
-//				for(int i=0; i<jsonarray.length(); i++)
-//				{
-//					JSONObject articleobj = new JSONObject(jsonarray.get(i).toString());
-//					
-//					if(articleobj.has("article_id")) {
-//						idTmp = articleobj.getString("article_id");
-//						// ==== Collection
-//						//System.out.println(currentFileName+"	"+idTmp+"	"+startPoint);
-//						filenameVec.add(currentFileName);
-//						articleIdVec.add(idTmp);
-//					}
-//				}
-//			}
-//		}
-//	}
-//	
-//	private void articleIndex(String historyfileName, String historyarticleId, String currentFileName) throws Exception
-//	{
-//		String Line = "";
-//		FileReader fr = new FileReader(Units.articleFolder + historyfileName);
-//		BufferedReader bfr = new BufferedReader(fr);
-//		
-//		String strTmp = "";
-//		while((Line = bfr.readLine())!=null)
-//		{	
-//			strTmp += Line;
-//		}
-//		fr.close();
-//		bfr.close();
-//		
-//		String idTmp;
-//		if(isJSONValid(strTmp)) {
-//			JSONObject obj = new JSONObject(strTmp);
-//			if(obj.has("articles")) {
-//				JSONArray jsonarray = new JSONArray(obj.get("articles").toString());
-//				for(int i=0; i<jsonarray.length(); i++)
-//				{
-//					JSONObject articleobj = new JSONObject(jsonarray.get(i).toString());
-//					if(articleobj.has("article_id")) {
-//						idTmp = articleobj.getString("article_id");
-//						
-//						if((filestartPoint == true) && (startPoint == true)) {
-//							// ==== Collection
-//							//System.out.println(currentFileName+"	"+idTmp+"	"+startPoint);
-//							filenameVec.add(currentFileName);
-//							articleIdVec.add(idTmp);
-//						}
-//						
-//						if((filestartPoint == true) && (idTmp.equalsIgnoreCase(historyarticleId))) {
-//							startPoint = true;
-//						}
-//					}
-//				}
-//			}
-//		}
-//	}
 	
 	private boolean isJSONValid(String jsonInString) {
 		
@@ -607,6 +543,84 @@ public class DataTagging_Frame {
 		}
 	}
 	
+	private void getValueAverageByarticleId(String articleId, String dateStr, String addtwoday) throws Exception 
+	{
+		File file = new File(Units.value_folder + articleId + Units.extension);
+		if (file.exists()) {
+			BufferedReader bfr = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			String Line;
+			String temp[];
+			int dateindexTag = 0;
+			int dateindextwoTag = 0;
+			int index = 0;
+			allvalueVec.clear();
+			while ((Line = bfr.readLine()) != null) {
+				temp = Line.split("	");
+				if(dateStr.equalsIgnoreCase(temp[0])) {
+					dateindexTag = index;
+					//break;
+				}
+				if(addtwoday.equalsIgnoreCase(temp[0])) {
+					dateindextwoTag = index;
+				}
+				allvalueVec.add(temp[1]);
+				index++;
+			}
+			bfr.close();
+			
+			int nextIndex;
+			if(dateindexTag > dateindextwoTag) {
+				nextIndex = dateindexTag;
+			}else {
+				nextIndex = dateindextwoTag;
+			}
+			
+			// values average
+			onemonthAverage = monthAverage(1, nextIndex, allvalueVec);
+			twomonthAverage = monthAverage(2, nextIndex, allvalueVec);
+			threemonthAverage = monthAverage(3, nextIndex, allvalueVec);
+		}
+	}
+	
+	private double monthAverage(int monthInt, int dateIndex, Vector allvalueVec)
+	{
+		int remnants = allvalueVec.size() - dateIndex;
+		double value1 = 0;
+		double value2 = 0;
+		double value3 = 0;
+		double value = 0; 
+		if(monthInt == 1) {
+			if(remnants < 22) {
+				dateremindTag = "less than 30 days";
+			}else {
+				for(int i=0; i<5; i++) {
+					value1 += Double.parseDouble(allvalueVec.get(22+i+dateIndex).toString());
+				}
+				value = value1 / 5;
+			}
+		}else if(monthInt == 2) {
+			if(remnants < 46) {
+				dateremindTag = "less than 60 days";
+			}else {
+				for(int i=0; i<5; i++) {
+					value2 += Double.parseDouble(allvalueVec.get(46+i+dateIndex).toString());
+				}
+				value = value2/ 5;
+			}
+		}else if(monthInt == 3) {
+			if(remnants < 66) {
+				dateremindTag = "less than 90 days";
+			}else {
+				for(int i=0; i<5; i++) {
+					value3 += Double.parseDouble(allvalueVec.get(66+i+dateIndex).toString());
+				}
+				value = value3 / 5;
+			}
+		}
+		
+		return value;
+	}
+	
 	private void ReadCompany() throws Exception {
 		// TWSE
 		ReadCompany(Units.sourceFolder, Units.twsefile);
@@ -629,6 +643,122 @@ public class DataTagging_Frame {
 
 			bfr.close();
 		}
+	}
+	
+	private String ISODateParser(String dateStr) throws Exception {
+		boolean chinesecheck;
+		chinesecheck = isChinese(dateStr);
+		DateTimeFormatter formatter;
+		if (chinesecheck == true) {
+			formatter = DateTimeFormatter.ofPattern(Units.isotime_pattern, Locale.TAIWAN);
+		} else {
+			formatter = DateTimeFormatter.ofPattern(Units.isotime_pattern, Locale.ENGLISH);
+		}
+
+		LocalDate dateTime = LocalDate.parse(dateStr, formatter);
+		
+		return dateTime.toString().replaceAll("-", "");
+	}	
+
+ 	private String ISODateParserZone(String dateStr) throws Exception {
+		boolean chinesecheck;
+		chinesecheck = isChinese(dateStr);
+		DateTimeFormatter formatter;
+		if (chinesecheck == true) {
+			formatter = DateTimeFormatter.ofPattern(Units.isotime_patternZone, Locale.TAIWAN);
+		} else {
+			formatter = DateTimeFormatter.ofPattern(Units.isotime_patternZone, Locale.ENGLISH);
+		}
+
+		LocalDate dateTime = LocalDate.parse(dateStr, formatter);
+		
+		return dateTime.toString().replaceAll("-", "");
+	}
+ 	
+ 	private boolean isChinese(String con) {
+		for (int i = 0; i < con.substring(0, 3).length(); i++) {
+			if (!Pattern.compile("[\u4e00-\u9fa5]").matcher(String.valueOf(con.charAt(i))).find()) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+	
+	private String replaceSpace(String dateStr) {
+		String dategap = "0";
+		String front = dateStr.substring(0, 8);
+		String last = dateStr.substring(9, dateStr.length());
+		String newdateStr = "";
+		if(dateStr.substring(8, 9).equalsIgnoreCase(" ")) {
+			newdateStr = front + dategap + last;
+		}else {
+			newdateStr = dateStr;
+		}
+		
+		return newdateStr;
+	}
+	
+	private String convertTWDate(String AD) {
+	    SimpleDateFormat df4 = new SimpleDateFormat("yyyyMMdd");
+	    SimpleDateFormat df2 = new SimpleDateFormat("MMdd");
+	    Calendar cal = Calendar.getInstance();
+	    String TWDate = "";
+	    try {
+	        cal.setTime(df4.parse(AD));
+	        cal.add(Calendar.YEAR, -1911);
+	        TWDate = Integer.toString(cal.get(Calendar.YEAR)) + df2.format(cal.getTime());
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        //return null;
+	    }
+	    
+	    return TWDate;
+	}
+	
+	private String addTwoDate(String dateStr) throws Exception
+	{
+		DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+		Date inputDate = dateFormat.parse(dateStr);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(inputDate);
+		int inputDayOfYear = cal.get(Calendar.DAY_OF_YEAR);
+		cal.set(Calendar.DAY_OF_YEAR, inputDayOfYear + 2);
+		
+		return cal.getTime().toString();
+		//return cal.getTime();
+	}
+	
+	private void DisplayAndClean()
+	{
+		lblNewLabel_2.setText(date);
+		lblNewLabel_3.setText(author);
+		lblNewLabel_6.setText(title);
+		textPane_1.setText("");
+		textPane_2.setText("");
+		textPane_3.setText("");
+		textPane.setText("");
+		lblNewLabel_6.setEnabled(false);
+		
+		messagesCount = 0;
+		companyIdDisplay.clear();
+		companyNameDisplay.clear();
+		valueDisplay.clear();
+		articleId = "";
+		author = "";
+		title = "";
+		content = "";
+		date = "";
+		inputarticleId = "";
+		formatdate = "";
+		TWDate = "";
+		onemonthAverage = 0;
+		twomonthAverage = 0;
+		threemonthAverage = 0;
+		dateremindTag = "";
+		formatdateAdd = "";
+		TWDateAdd = "";
+		addtwpday = "";
 	}
 	
 }
