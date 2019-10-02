e9e13971806b99e7fa714fc6251ef1c5bc8e9812
Chao-Hsuan Ke <chke@iir.csie.ncku.edu.tw>
2019-10-02 23:34:05 +0800

    get stock value by date

--- src/main/java/ValuesProcessing/GetValues_Main.java
+++ src/main/java/ValuesProcessing/GetValues_Main.java
@@ -3,7 +3,7 @@ package ValuesProcessing;
 /*
  * download values (Main)
  * version: July 03, 2019 07:23 PM
- * Last revision: October 01, 2019 11:28 PM
+ * Last revision: October 02, 2019 09:10 PM
  * 
  * Author : Chao-Hsuan Ke
  * E-mail : phelpske.dev at gmail dot com
@@ -13,6 +13,7 @@ package ValuesProcessing;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -20,6 +21,9 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Vector;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class GetValues_Main
 {
@@ -36,21 +40,63 @@ public class GetValues_Main
 	// 
 	Vector companyId = new Vector();
 	
-	GetValueandProcessing value;
+	GetValueandProcessingByStockId value;
 	
 	public GetValues_Main() throws Exception
 	{
-		// by stock id
+		// get Stock Value
 		GetStockValue();
+		// https://www.twse.com.tw/exchangeReport/MI_INDEX?response=json&date=20191001&type=IND
+		// 發行量加權股價指數
 		
-		// Stock Index
-			//https://www.twse.com.tw/exchangeReport/MI_INDEX?response=json&date=20191001&type=IND
-			//發行量加權股價指數
-			
+		// by stock id
+//		GetStockValuebyId();
 		
 	}
 	
 	private void GetStockValue() throws Exception
+	{
+		String startDate = "20180101";
+		GetValueandProcessing_StockValue stockvalue = new GetValueandProcessing_StockValue(startDate);
+		
+		
+//		BufferedReader bfr;
+//		String Line = "";
+//		FileReader fr = new FileReader("/Users/phelps/Desktop/" + "20191002.txt");
+//		bfr = new BufferedReader(fr);
+//				
+//		while((Line = bfr.readLine())!=null)
+//		{			
+//			JSONObject obj = new JSONObject(Line);
+//			
+//			// Value
+//			if(obj.has("params")) {
+//				JSONObject objPara = new JSONObject(obj.get("params").toString());
+//				if(objPara.has("date")) {
+//					System.out.println(objPara.get("date"));
+//				}
+//			}
+//			
+//			
+//			if(obj.has("data3")) {
+//				JSONArray jsonarray = new JSONArray(obj.get("data1").toString());
+//				for(int i=0; i<jsonarray.length(); i++)
+//				{
+//					//System.out.println(jsonarray.get(i));
+//					JSONArray arrayData = new JSONArray(jsonarray.get(i).toString());
+//					if(arrayData.get(0).toString().equalsIgnoreCase("發行量加權股價指數")) {
+//						System.out.println(arrayData.get(i));
+//					}
+//				}
+//			}
+//		}
+//		fr.close();
+//		bfr.close();
+		
+	}
+	
+	
+	private void GetStockValuebyId() throws Exception
 	{
 		Today();
 		
@@ -59,7 +105,7 @@ public class GetValues_Main
 		ReadCompany(twseStr);
 		for(int i=0; i<companyId.size(); i++)
 		{
-			GetValueandProcessing value = new GetValueandProcessing(companyId.get(i).toString(), "twse");
+			GetValueandProcessingByStockId value = new GetValueandProcessingByStockId(companyId.get(i).toString(), "twse");
 		}
 		
 		// TPEX
@@ -68,7 +114,7 @@ public class GetValues_Main
 		ReadCompany(tpexStr);
 		for(int i=0; i<companyId.size(); i++)
 		{
-			GetValueandProcessing value = new GetValueandProcessing(companyId.get(i).toString(), "tpex");
+			GetValueandProcessingByStockId value = new GetValueandProcessingByStockId(companyId.get(i).toString(), "tpex");
 		}
 	}
 	
